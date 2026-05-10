91bd9ad20a443513b4eb82ee2a3782292f8d2532
Santiago244 <santiagogonzalezcuellaratgmail.com>
2026-05-11 01:53:45 +0200

    train comments

--- src/train.py
+++ src/train.py
@@ -181,6 +181,7 @@ def train(
             optimizer=optimizer,
             desc=f"Epoch {epoch}/{epochs} train",
         )
+        # Validation is done after each epoch but without gradient updates to track best model by val accuracy and enable early stopping.
         val_metrics = run_epoch(
             model,
             val_loader,
